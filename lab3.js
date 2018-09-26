cc5e96734a7841c09680d26ed74438bf576e6f99
vitalok4 <43232574+vitalok4@users.noreply.github.com>
2018-09-26 14:07:18 +0300

    Add files via upload

---
+++ lab3.js
@@ -0,0 +1,21 @@
+﻿function $() 
+{ var elements = new Array(); 
+for (var i = 0; i < arguments.length; i++) 
+{ var element = arguments[i]; 
+if (typeof element == 'string') 
+element = document.getElementById(element); 
+if (arguments.length == 1) 
+return element; 
+elements.push(element); 
+} 
+return elements; } 
+
+function f1()
+{ 
+document.body.style.backgroundColor="pink";
+}
+
+function f2()
+{ 
+document.getElementById("b1").style.color="blue";
+}
