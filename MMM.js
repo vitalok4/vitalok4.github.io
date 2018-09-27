ab23456c0bb17fde2809654d5f239582c06dae04
vitalok4 <43232574+vitalok4@users.noreply.github.com>
2018-09-27 12:21:41 +0300

    Add files via upload

---
+++ MMM.js
@@ -0,0 +1,26 @@
+﻿function $() 
+{ var elements = new Array(); 
+for (var i = 0; i < arguments.length; i++) 
+{ var element = arguments[i]; 
+if (typeof element == 'string') 
+element = document.getElementById(element); 
+if (arguments.length == 1) 
+return element; 
+elements.push(element); 
+} 
+return elements; }
+
+function f1(){
+	a=parseFloat($('tx1').value);
+	if(a>0)
+		{ x1=a+1;}
+	else 
+ 		{
+		if(a<0)
+		{ x1=a-2;}
+
+ 		else
+ 		{x1=10;}
+		}
+$('tx2').value=x1;}
+		
