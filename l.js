f6ba44a4889f5f5d1859ddb5bf80a3ce3a44cbd7
vitalok4 <43232574+vitalok4@users.noreply.github.com>
2018-11-28 19:31:36 +0200

    Add files via upload

---
+++ l.js
@@ -0,0 +1,19 @@
+﻿var t;
+var b1=255;
+var g1=55;
+var r1=153;
+function f()
+{
+var x1=document.getElementById('b1');
+x1.style.left=(parseInt(x1.style.left)+70)+'px';
+x1.style.down=(parseInt(x1.style.top)+50)+'px';
+b1-=200;
+str1='rgb('+b1+', '+g1+', '+r1+')';
+x1.style.backgroundColor=str1;
+}
+function k()
+{t=setInterval('f()',300);}
+function g()
+{
+clearInterval(t);
+}
