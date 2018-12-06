7f5ffbe7e2af12626ae8f6d5fd81cd2e8f807464
vitalok4 <43232574+vitalok4@users.noreply.github.com>
2018-12-06 12:40:36 +0200

    Update l.js

--- l.js
+++ l.js
@@ -7,7 +7,9 @@ function f()
 var x1=document.getElementById('b11');
 x1.style.left=(parseInt(x1.style.left)+70)+'px';
 x1.style.down=(parseInt(x1.style.top)+50)+'px';
-b1-=200;
+b1-=150;
+g1-=1000;
+r1-=90;
 str1='rgb('+b1+', '+g1+', '+r1+')';
 x1.style.backgroundColor=str1;
 }
