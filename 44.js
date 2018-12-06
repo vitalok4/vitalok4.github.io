e6e649839e7c19cad9df454463d6d1045dbab658
vitalok4 <43232574+vitalok4@users.noreply.github.com>
2018-12-06 13:26:51 +0200

    Add files via upload

---
+++ 44.js
@@ -0,0 +1,37 @@
+﻿function $()
+
+{ var elements = new Array(); 
+
+for (var i = 0; i < arguments.length; i++) 
+
+{ var element = arguments[i]; 
+
+if (typeof element == 'string') 
+element = document.getElementById(element); 
+
+if (arguments.length == 1) 
+return element; 
+elements.push(element); 
+} 
+return elements; }
+
+
+
+function f(x)
+	
+		{
+		
+			var st = "0123456789.";
+		
+			var mass = x.split('.');
+		
+			f = "False";
+		
+			if(mass.length==2)
+			
+				f="Okey";
+		
+				return f;
+		
+
+}
