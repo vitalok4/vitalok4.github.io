1839be33d007712704cc882deb803d45e5d43dee
vitalok4 <43232574+vitalok4@users.noreply.github.com>
2018-12-13 11:40:25 +0200

    Add files via upload

---
+++ lab45.js
@@ -0,0 +1,24 @@
+﻿function $() 
+{ var elements = new Array(); 
+for (var i = 0; i < arguments.length; i++) 
+{ var element = arguments[i]; 
+if (typeof element == 'string') 
+element = document.getElementById(element); 
+if (arguments.length == 1) 
+return element; 
+elements.push(element); 
+} 
+return elements; }
+
+function f2()
+{       K=parseFloat($('tx3').value);
+	N=parseFloat($('tx1').value);
+K=0
+while(K*K<N)
+{K=K+1;
+}
+if(K*K>N)
+{K=K-1;
+}
+$('tx3').value=''+K;
+}
